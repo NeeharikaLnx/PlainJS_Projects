a2ca78943e6c0b0c14a74e1a5f582bfe70dc2eb7
NeeharikaLnx <niharikaa972@gmail.com>
2025-11-06 11:53:31 -0500

    Add files via upload

--- callback.js
+++ callback.js
@@ -23,3 +23,33 @@ function greet1(callback){
 greet1(function(){
    console.log("Hi there")
 });
+
+
+//Arrow Function
+
+function greet3(callback){
+    console.log("using arrow function");
+    callback();
+    console.log("after callback");
+}
+
+greet3(()=>console.log("Hello world"));
+
+
+//Callbacks with parameters
+
+function greet4(name,callback){
+    console.log("Hello " + name);
+    callback();
+}
+
+greet4("Neeha",function(){
+    console.log("Good bye");
+});
+
+
+//Call Back inside built in Javascript function
+
+setTimeout(function (){
+    console.log("This runs after 2 seconds");
+},2000);
